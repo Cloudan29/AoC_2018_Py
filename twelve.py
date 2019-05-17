3d74e0b644d83022c03cd739549ece2425d121af
Cloudan29 <39637967+Cloudan29@users.noreply.github.com>
2019-05-17 14:31:55 -0400

    Add files via upload

---
+++ twelve.py
@@ -0,0 +1,59 @@
+def calculate_sum(state, offset):
+    sum = 0
+    for i in range(len(state)):
+        if state[i] == '#':
+            sum += i - offset
+
+    return sum
+
+def calculate_gen(gens, rules, pots):
+    for _ in range(gens):
+        nextGen = '..'
+
+        for j in range(len(pots) - 5):
+            current = pots[j:j+5]
+            for rule in rules:
+                if current == rule[0]:
+                    nextGen += rule[1]
+                    break
+
+        pots = nextGen + '....'
+
+    return pots
+
+def find_pattern(pots,rules,offset):
+    last_pots = pots
+    for i in range(1,140):
+        new_pots = calculate_gen(1,rules,last_pots)
+        print (calculate_sum(new_pots,offset) - calculate_sum(last_pots,offset), ':', i, ':', calculate_sum(new_pots,offset))
+        last_pots = new_pots
+
+def main():
+    pots = "#...#####.#..##...##...#.##.#.##.###..##.##.#.#..#...###..####.#.....#..##..#.##......#####..####..."
+    offset = 2
+
+    # Creating starting state
+    for _ in range(offset):
+        pots += '.'
+        pots = '.' + pots
+
+    # Creating array of rule => new state
+    input = open('twelve.txt')
+    rules = []
+    for line in input:
+        if line[0] == '.' or line[0] == '#':
+            rules.append((line[:5],line[9]))
+    
+    '''
+    Find where pattern emerges
+    find_pattern(pots,rules,offset)
+    sum: 11873, gen: 134, pattern: +86
+    '''
+
+    pots = calculate_gen(20,rules,pots)
+    print (calculate_sum(pots,offset))
+    print (11873 + (50000000000 - 134) * 86)
+
+
+if __name__ == '__main__':
+    main()
