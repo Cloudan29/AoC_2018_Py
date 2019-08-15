82ce173dae7e45bb4135fe04520bd634d2d3b157
Daniel Cloutier <guitarman.dan29@gmail.com>
2019-08-14 23:28:41 -0400

    now day5 is done

--- day5.py
+++ day5.py
@@ -1,20 +1,33 @@
 import string
 
 def part1(polymer):
-    flag = True
-    while flag:
-        flag = False
-        for i in range(len(polymer)-1):
+    flag = False
+    i = 1
+    while i < len(polymer) - 1:
+        if polymer[i].isupper():
+            if polymer[i+1] == polymer[i].lower():
+                polymer = polymer[:i] + polymer[i+2:]
+                flag = True
+        else:
+            if polymer[i+1] == polymer[i].upper():
+                polymer = polymer[:i] + polymer[i+2:]
+                flag = True
+        if i < len(polymer):
             if polymer[i].isupper():
-                if polymer[i+1] == polymer[i].lower():
-                    polymer = polymer[:i] + polymer[i+2:]
+                if polymer[i-1] == polymer[i].lower():
+                    polymer = polymer[:i-1] + polymer[i+1:]
                     flag = True
-                    break
             else:
-                if polymer[i+1] == polymer[i].upper():
-                    polymer = polymer[:i] + polymer[i+2:]
+                if polymer[i-1] == polymer[i].upper():
+                    polymer = polymer[:i-1] + polymer[i+1:]
                     flag = True
-                    break
+
+        if flag:
+            flag = False
+            if i > 1:
+                i -= 1
+            continue
+        i += 1
 
     return len(polymer)
 
@@ -23,9 +36,9 @@ def part2():
     low_alpha = string.ascii_lowercase
     high_alpha = string.ascii_uppercase
     lengths = []
-    polymer = open("inputs/day5.txt").read()
-    for _ in range(len(low_alpha)):
-        lengths.append(part1(polymer.replace(low_alpha, '').replace(high_alpha, '')))
+    for i in range(len(low_alpha)):
+        polymer = open("inputs/day5.txt").read().replace(low_alpha[i], '').replace(high_alpha[i], '')
+        lengths.append(part1(polymer))
     return min(lengths)
 
 
