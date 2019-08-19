f460266c50728176cb0471dd5622572505303cc5
Daniel Cloutier <guitarman.dan29@gmail.com>
2019-08-18 20:33:02 -0400

    completed day6

--- day6.py
+++ day6.py
@@ -9,16 +9,62 @@ with open("inputs/day6.txt") as inp:
 
 # Create the 2d list of tuples (closest_coordinate, sum_of_distances)
 # Where -1 is a tie for closest_coordinate
-AREA_SIZE = 1000
+AREA_SIZE = 500
 play_area = [[[-1, 0] for i in range(AREA_SIZE)] for j in range(AREA_SIZE)]
 for i in range(len(play_area)):
     for j in range(len(play_area[i])):
+        distances = []
         for k in coordinates:
-            play_area[i][j][1] += abs(i - coordinates[k][0]) + abs(j - coordinates[k][1])
+            distance = abs(i - coordinates[k][0]) + abs(j - coordinates[k][1])
+            play_area[i][j][1] += distance
+            
+            if len(distances) == 0:
+                play_area[i][j][0] = k
+            elif distance == min(distances):
+                play_area[i][j][0] = -1
+            elif distance < min(distances):
+                play_area[i][j][0] = k
+
+            distances.append(distance)
 
 
 def part1():
-    return
+    infinite_areas = [-1]
+
+    # Left side
+    for left in play_area[0]:
+        if not left[0] in infinite_areas:
+            infinite_areas.append(left[0])
+
+    # Right side
+    for right in play_area[-1]:
+        if not right[0] in infinite_areas:
+            infinite_areas.append(right[0])
+
+    # Top and bottom
+    for col in play_area:
+        if not col[0][0] in infinite_areas:
+            infinite_areas.append(col[0][0])
+        if not col[-1][0] in infinite_areas:
+            infinite_areas.append(col[-1][0])
+
+    # Non infinite areas
+    finite_areas = {}
+    for coor in coordinates:
+        if not coor in infinite_areas:
+            finite_areas[coor] = 0
+
+    for i in range(len(play_area)):
+        for j in range(len(play_area[i])):
+            if play_area[i][j][0] in finite_areas:
+                finite_areas[play_area[i][j][0]] += 1
+
+    ans = 0
+    for area in finite_areas:
+        if finite_areas[area] > ans:
+            ans = finite_areas[area]
+            
+    return ans
 
 
 def part2():
